6543a8301a786596673ef2459c088542df438da9
pavanigarlapati <pavanigarlapati05@gmail.com>
2025-12-17 23:58:52 +0530

    Q4

---
+++ UserProfile.jsx
@@ -0,0 +1,15 @@
+import UserInfo from "./UserInfo";
+
+function UserProfile() {
+  const name = "Alice";
+  const age = 22;
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <UserInfo name={name} age={age} />
+    </div>
+  );
+}
+
+export default UserProfile;
