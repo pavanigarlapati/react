d5353238a72dec989207066c06083e72501c8828
pavanigarlapati <pavanigarlapati05@gmail.com>
2025-12-17 23:52:43 +0530

    Q3

---
+++ MessageCard.jsx
@@ -0,0 +1,18 @@
+function MessageCard({ title, message }) {
+  return (
+    <div
+      style={{
+        border: "1px solid #ccc",
+        padding: "16px",
+        margin: "10px",
+        borderRadius: "8px",
+        width: "300px",
+      }}
+    >
+      <h3>{title}</h3>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default MessageCard;
